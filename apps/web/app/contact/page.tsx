a5d6550d587e6d006defcf2c427fd264fb882441
PratY7728 <152558066+PratY7728@users.noreply.github.com>
2024-06-24 22:08:35 +0530

    added contact us page

---
+++ apps/web/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ContactUshero from '../../components/ContactUshero'
+import ContactForm from '../../components/ContactUsform'
+
+const page = () => {
+  return (
+    <div>
+      <ContactForm/>
+    </div>
+  )
+}
+
+export default page
--- apps/web/components/Navbar.tsx
+++ apps/web/components/Navbar.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRouter } from "next/navigation";
 // import React, { useEffect, useState } from 'react';
 
 // const NavBar = () => {
@@ -240,6 +241,7 @@
 import React, { useEffect, useState } from 'react';
 
 const NavBar = () => {
+  const router =useRouter();
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const [activeDropdown, setActiveDropdown] = useState<string | null>(null);
 
@@ -456,7 +458,7 @@ const NavBar = () => {
             className="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-500 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 focus:outline-none focus:shadow-outline"
             href="#"
           >
-            Contact
+           <button   onClick={() => router.push("/contact")}>Contact</button>
           </a>
         </nav>
       </div>
