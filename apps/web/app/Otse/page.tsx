2d94b44ccf42344966097aa61c74e08b917726f6
SId-pranay123 <siddharthpranay0@gmail.com>
2024-08-04 18:12:49 +0530

    changes

--- apps/web/app/Otse/page.tsx
+++ apps/web/app/Otse/page.tsx
@@ -1,59 +1,90 @@
 
-import React from 'react'
-import ScholarshipForm from '../../components/ScholarshipForm'
-import Image from 'next/image'
+// import React from 'react'
+// import ScholarshipForm from '../../components/ScholarshipForm'
+// import Image from 'next/image'
+
+// const page = () => {
+//   return (
+//     <div>
+//         <ScholarshipForm/>
+
+//         <div className='p-12 text-center'>
+//           <h1 className="text-3xl font-bold ">Omega Talent Seacrh Examination</h1>
+//           <p className='max-w-3xl mx-auto py-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+//         </div>
+
+//         <div className='grid lg:grid-cols-2 sm:grid-cols-1 px-10 pb-8'>
+//           <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] '>
+//               <Image src={"/otse1.jpeg"} alt="logo" layout='fill' objectFit="cover" />
+//             </div>
+//           </div>
+//           <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] '>
+//             <Image src={"/otse2.jpeg"} alt="logo" width={300} height={300}/>
+//             </div>
+//           </div>
+//           {/* <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] bg-red-500 '>
+//               <Image src={""} alt="logo" width={300} height={300} />
+//             </div>
+//           </div>
+//           <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] bg-red-500 '>
+//               <Image src={""} alt="logo" width={300} height={300} />
+//             </div>
+//           </div>
+//           <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] bg-red-500 '>
+//               <Image src={""} alt="logo" width={300} height={300} />
+//             </div>
+//           </div>
+//           <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] bg-red-500 '>
+//               <Image src={""} alt="logo" width={300} height={300} />
+//             </div>
+//           </div>
+//           <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
+//             <div className='w-[92%] h-[85%] bg-red-500 '>
+//               <Image src={""} alt="logo" width={300} height={300} />
+//             </div>
+//           </div>
+          
+//            */}
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default page
+
+import React from 'react';
+import ScholarshipForm from '../../components/ScholarshipForm';
 
 const page = () => {
   return (
     <div>
-        <ScholarshipForm/>
+        <ScholarshipForm />
 
         <div className='p-12 text-center'>
-          <h1 className="text-3xl font-bold ">Omega Talent Seacrh Examination</h1>
-          <p className='max-w-3xl mx-auto py-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <h1 className="text-3xl font-bold">Omega Talent Search Examination</h1>
+          <p className='max-w-3xl mx-auto py-8'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
 
         <div className='grid lg:grid-cols-2 sm:grid-cols-1 px-10 pb-8'>
-          <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] '>
-              <Image src={"/otse1.jpeg"} alt="logo" width={300} height={300} />
+          <div className='m-4 rounded-xl shadow-xl'>
+            <div className='w-full h-full relative rounded-xl overflow-hidden'>
+              <img src="/otse1.jpeg" alt="logo" className='p-4 w-full h-full object-cover rounded-xl'/>
             </div>
           </div>
-          <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] '>
-            <Image src={"/otse2.jpeg"} alt="logo" width={300} height={300}/>
+          <div className='m-4 rounded-xl shadow-xl'>
+            <div className='w-full h-full relative rounded-xl overflow-hidden'>
+              <img src="/otse2.jpeg" alt="logo" className='p-4 w-full h-full object-cover rounded-xl'/>
             </div>
           </div>
-          {/* <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] bg-red-500 '>
-              <Image src={""} alt="logo" width={300} height={300} />
-            </div>
-          </div>
-          <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] bg-red-500 '>
-              <Image src={""} alt="logo" width={300} height={300} />
-            </div>
-          </div>
-          <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] bg-red-500 '>
-              <Image src={""} alt="logo" width={300} height={300} />
-            </div>
-          </div>
-          <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] bg-red-500 '>
-              <Image src={""} alt="logo" width={300} height={300} />
-            </div>
-          </div>
-          <div className=' m-4 flex justify-center rounded-xl shadow-xl gap-4 items-center'>
-            <div className='w-[92%] h-[85%] bg-red-500 '>
-              <Image src={""} alt="logo" width={300} height={300} />
-            </div>
-          </div>
-          
-           */}
         </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
