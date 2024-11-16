c0a4d8b11f89484c84c71d12594461ae39ae9d73
SId-pranay123 <siddharthpranay0@gmail.com>
2024-11-16 14:17:28 +0530

    Latest push

--- apps/web/components/Hero.tsx
+++ apps/web/components/Hero.tsx
@@ -3,29 +3,7 @@ import Image from 'next/image';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 
-// const responsive = {
-//     superLargeDesktop: {
-//         // the naming can be any, depends on you.
-//         breakpoint: { max: 4000, min: 3000 },
-//         items: 1,
-//         slidesToSlide: 1
-//     },
-//     desktop: {
-//         breakpoint: { max: 3000, min: 1024 },
-//         items: 1,
-//         slidesToSlide: 1
-//     },
-//     tablet: {
-//         breakpoint: { max: 1024, min: 464 },
-//         items: 1,
-//         slidesToSlide: 1
-//     },
-//     mobile: {
-//         breakpoint: { max: 464, min: 0 },
-//         items: 1,
-//         slidesToSlide: 1
-//     }
-// }
+
 const responsive = {
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
@@ -58,17 +36,6 @@ const Hero = () => {
                 responsive={responsive}
                 itemClass='item'
             >
-            {/* <div className='w-[100vw] h-[88vh] lg:flex md:flex items-center justify-around bg-orange-300 md:clip_path'>
-                <div className="grid grid-rows-1 justify-center items-center gap-[2rem] my-auto">
-                    <Image 
-                        src="/Banner.jpg"
-                        alt="omega"
-                        width={1350}
-                        height={700}
-                        // className='py-4'
-                    />
-                </div>
-            </div> */}
             <div className='w-screen h-screen lg:flex md:flex items-center justify-around bg-[#560002]'>
                 <div className="flex justify-center items-center w-full h-full">
                     <Image 
