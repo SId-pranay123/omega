51171fb354a1287d2d5539bca58fede442e5be9e
Shashi Jha <Shashi.jha@sapiens.com>
2024-11-21 23:51:11 +0530

    Fixed Responsiveness

--- apps/web/components/CourseDescription.tsx
+++ apps/web/components/CourseDescription.tsx
@@ -2,38 +2,36 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 interface CourseDescriptionProps {
-    title: string
-    description: string
-    highlights: string[]
+    title: string;
+    description: string;
+    highlights: string[];
 }
 
-const CourseDescription : React.FC<CourseDescriptionProps> = ({title, description, highlights}) => {
+const CourseDescription: React.FC<CourseDescriptionProps> = ({ title, description, highlights }) => {
     return (
-        <div  >
-            <div className=" p-6">
+        <div>
+            <div className="p-6 text-center">
                 <h1 className="text-3xl font-bold mb-4">Course Description of {title}</h1>
                 <p className="text-lg mb-4">{description}</p>
                 <h2 className="text-2xl font-bold mb-4">Course Highlights</h2>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                    {highlights.map((highlight, index) => ( 
-                        <div key={index} className="bg-white flex text-lg mb-4 rounded-xl min-h-10">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 max-w-4xl mx-auto">
+                    {highlights.map((highlight, index) => (
+                        <div
+                            key={index}
+                            className="bg-white flex items-center justify-center text-base md:text-lg mb-4 rounded-xl min-h-20 w-full shadow-lg"
+                        >
                             <div className="p-4">
                                 <span style={{ color: 'green', fontSize: '24px' }}>
                                     <FontAwesomeIcon icon={faCheck} />
                                 </span>
                             </div>
-                            <div className="p-4">
-                                {highlight}
-                            </div>
+                            <div className="p-4">{highlight}</div>
                         </div>
                     ))}
                 </div>
-                
             </div>
-            
         </div>
-    )
-}
+    );
+};
 
-
-export default CourseDescription
+export default CourseDescription;
